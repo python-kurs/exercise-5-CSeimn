dcbbd5cc066bdc735ae1857f46439e0a924f709c
CSeimn <44396406+CSeimn@users.noreply.github.com>
2019-06-17 12:20:27 +0200

    first_attempt

--- main.py
+++ main.py
@@ -5,18 +5,20 @@ import pandas as pd
 import xarray as xr
 import matplotlib.pyplot as plt
 
-input_dir  = Path("data")
+input_dir  = Path("../Data/")
+data_dir = input_dir / "tg_ens_mean_0.25deg_reg_v19.0e.nc" 
 output_dir = Path("solution")
 
 # 1. Go to http://surfobs.climate.copernicus.eu/dataaccess/access_eobs.php#datafiles
 #    and download the 0.25 deg. file for daily mean temperature.
 #    Save the file into the data directory but don't commit it to github!!! [2P]
 
-
 # 2. Read the file using xarray. Get to know your data. What's in the file?
 #    Calculate monthly means for the reference periode 1981-2010 for Europe (Extent: Lon_min:-13, Lon_max: 25, Lat_min: 30, Lat_max: 72). [2P]
-
-
+data = xr.open_dataset(data_dir)
+data_ref = data.sel(latitude = slice(30, 72), longitude = slice(-13, 25), time = slice("1981-01-01", "2010-12-31"))
+data_nona = data_ref.fillna(value = "na")
+data_gr = 
 # 3. Calculate monthly anomalies from the reference period for the year 2018 (use the same extent as in #2).
 #    Make a quick plot of the anomalies for the region. [2P]
 
